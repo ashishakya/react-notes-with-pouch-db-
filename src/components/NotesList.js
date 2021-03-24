80d9624a0564582a7e1936423d493ce64eedb28b
Ashish Shakya <ashishakya@outlook.com>
2021-03-24 22:50:31 +0545

    Routes Defintion
    - [x] Routes defined
    - [x] Define List and detail viewed

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function NotesList({notes}) {
     return (
@@ -6,7 +7,7 @@ export default function NotesList({notes}) {
             {notes.map(note => {
                return (
                     <div>
-                        <h2>{note.title}</h2>
+                        <Link to={`/notes/${note._id}`}><h2>{note.title}</h2></Link>
                     </div>
                 )
             })}
